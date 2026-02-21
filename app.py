a747371088b179648570b9e6e5f3303ccab0c0c5
aftab0045 <aftabattar0045@gmail.com>
2026-02-21 12:27:31 +0530

    add

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return 'successfully deployed python application through jenkins using CICD!!!!!!!!!, added webhook also for automation'
+    return 'successfully deployed python application through jenkins using CICD!!!!!!!!!, added webhook also for automation succefully deployed'
 @app.route('/hi')
 def hell():
     return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
